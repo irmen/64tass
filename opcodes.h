7e9461b24b3f07a02e1bd873162aa2ca812a4098
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2011-12-29 19:50:43 +0000

    Cleanup

--- opcodes.h
+++ opcodes.h
@@ -17,31 +17,14 @@
 */
 #ifndef _OPCODES_H_
 #define _OPCODES_H_
+#include <stdint.h>
 
-#define ADR_IMPLIED   0
-#define ADR_ACCU      1
-#define ADR_IMMEDIATE 2
-#define ADR_LONG      3
-#define ADR_ADDR      4
-#define ADR_ZP        5
-#define ADR_LONG_X    6
-#define ADR_ADDR_X    7
-#define ADR_ZP_X      8
-#define ADR_ADDR_X_I  9
-#define ADR_ZP_X_I    10
-#define ADR_ZP_S      11
-#define ADR_ZP_S_I_Y  12
-#define ADR_ADDR_Y    13
-#define ADR_ZP_Y      14
-#define ADR_ZP_LI_Y   15
-#define ADR_ZP_I_Y    16
-#define ADR_ADDR_LI   17
-#define ADR_ZP_LI     18
-#define ADR_ADDR_I    19
-#define ADR_ZP_I      20
-#define ADR_REL_L     21
-#define ADR_REL       22
-#define ADR_MOVE      23
+enum opr_e {
+    ADR_IMPLIED=0, ADR_ACCU, ADR_IMMEDIATE, ADR_LONG, ADR_ADDR, ADR_ZP,
+    ADR_LONG_X, ADR_ADDR_X, ADR_ZP_X, ADR_ADDR_X_I, ADR_ZP_X_I, ADR_ZP_S,
+    ADR_ZP_S_I_Y, ADR_ADDR_Y, ADR_ZP_Y, ADR_ZP_LI_Y, ADR_ZP_I_Y, ADR_ADDR_LI,
+    ADR_ZP_LI, ADR_ADDR_I, ADR_ZP_I, ADR_REL_L, ADR_REL, ADR_MOVE
+};
 
 #define OPCODES_65816 111
 #define OPCODES_6502 68
@@ -49,20 +32,20 @@
 #define OPCODES_6502i 98
 #define OPCODES_65DTV02 89
 #define ____ 0x42
-// 0x42 =WDM                  
-extern const unsigned char c65816[];
+// 0x42 =WDM
+extern const uint8_t c65816[];
 #define MNEMONIC65816 "adcandaslbccbcsbeqbgebitbltbmibnebplbrabrkbrlbvcbvsclccldcliclvcmpcopcpxcpydeadecdexdeyeorgccgcsgeqggegltgmignegplgragvcgvsinaincinxinyjmljmpjsljsrldaldxldylsrmvnmvpnoporapeapeiperphaphbphdphkphpphxphyplaplbpldplpplxplyreprolrorrtirtlrtssbcsecsedseisepstastpstxstystzswatadtastaxtaytcdtcstdatdctrbtsatsbtsctsxtxatxstxytyatyxwaixbaxce"
 //                      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100101102103104105106107108109110111
-extern const unsigned char c6502[];
+extern const uint8_t c6502[];
 #define MNEMONIC6502 "adcandaslbccbcsbeqbgebitbltbmibnebplbrkbvcbvsclccldcliclvcmpcpxcpydecdexdeyeorgccgcsgeqggegltgmignegplgvcgvsincinxinyjmpjsrldaldxldylsrnoporaphaphpplaplprolrorrtirtssbcsecsedseistastxstytaxtaytsxtxatxstya";
 //                     1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68
-extern const unsigned char c65c02[];
+extern const uint8_t c65c02[];
 #define MNEMONIC65C02 "adcandaslbccbcsbeqbgebitbltbmibnebplbrabrkbvcbvsclccldcliclvcmpcpxcpydeadecdexdeyeorgccgcsgeqggegltgmignegplgragvcgvsinaincinxinyjmpjsrldaldxldylsrnoporaphaphpphxphyplaplpplxplyrolrorrtirtssbcsecsedseistastxstystztaxtaytrbtsbtsxtxatxstya";
 //                      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79
-extern const unsigned char c6502i[];
+extern const uint8_t c6502i[];
 #define MNEMONIC6502i "adcahxalrancandanearraslasraxsbccbcsbeqbgebitbltbmibnebplbrkbvcbvsclccldcliclvcmpcpxcpydcmdcpdecdexdeyeorgccgcsgeqggegltgmignegplgvcgvsincinsinxinyisbiscjamjmpjsrlaelaslaxldaldsldxldylsrlxanoporaphaphpplaplprlarolrorrrartirtssaxsbcsbxsecsedseishashsshxshyslosrestastxstytastaxtaytsxtxatxstyaxaa";
 //                      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98
-extern const unsigned char c65dtv02[];
+extern const uint8_t c65dtv02[];
 #define MNEMONIC65DTV02 "adcalrandanearraslasrbccbcsbeqbgebitbltbmibnebplbrabrkbvcbvsclccldcliclvcmpcpxcpydcmdcpdecdexdeyeorgccgcsgeqggegltgmignegplgragvcgvsincinsinxinyisbiscjmpjsrlaxldaldxldylsrlxanoporaphaphpplaplprlarolrorrrartirtssacsaxsbcsecsedseisirslosrestastxstytaxtaytsxtxatxstyaxaa";
 //                        1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89
 
