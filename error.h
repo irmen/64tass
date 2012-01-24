b16d2a021b93c6383f071272689076effb33d900
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2012-01-24 22:28:13 +0000

    Some types

--- error.h
+++ error.h
@@ -75,7 +75,7 @@ extern void err_msg(enum errors_e, const char*);
 extern void err_msg2(enum errors_e, const char*, unsigned int);
 extern void err_msg_wrong_type(enum type_e, unsigned int);
 extern void freeerrorlist(int);
-extern void enterfile(const char *, uint32_t);
+extern void enterfile(const char *, line_t);
 extern void exitfile(void);
 extern void err_destroy(void);
 
