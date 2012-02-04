cca1b56f14b4a0bcc6cfbf14c7beb368145160c4
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2012-02-04 06:16:12 +0000

    Might be a different label in first pass...

--- trunk/eval.c
+++ trunk/eval.c
@@ -204,8 +204,10 @@ static void try_resolv_ident(struct value_s *val) {
 
 static void try_resolv_identref(struct value_s *val) {
     if (val->type == T_IDENTREF) {
-        if ((val->u.label->requires & current_provides)!=val->u.label->requires) err_msg(ERROR_REQUIREMENTS_,ident);
-        if (val->u.label->conflicts & current_provides) err_msg(ERROR______CONFLICT,ident);
+        if (pass != 1) {
+            if ((val->u.label->requires & current_provides)!=val->u.label->requires) err_msg(ERROR_REQUIREMENTS_,ident);
+            if (val->u.label->conflicts & current_provides) err_msg(ERROR______CONFLICT,ident);
+        }
         *val = val->u.label->value;
     } else if (val->type == T_UNDEF && pass == 1) val->type = T_NONE;
 }
