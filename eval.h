24cccd39bcbc94746b4cd36ee3d64f3a18e6a39c
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2012-08-26 17:03:20 +0000

    Propagate error the correctly

--- eval.h
+++ eval.h
@@ -9,6 +9,7 @@ extern void eval_finish(void);
 extern void set_uint(struct value_s *v, uval_t val);
 extern void set_int(struct value_s *v, ival_t val);
 extern uint_fast16_t petascii(size_t *, const struct value_s *);
+extern struct value_s error_value;
 
 struct values_s {
     struct value_s *val;
