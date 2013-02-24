cb35b18777b44608ffc068f2bcf06021688a3ef6
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2013-02-24 23:16:41 +0000

    Fix precedence again

--- eval.c
+++ eval.c
@@ -2364,23 +2364,23 @@ int get_exp(int *wd, int stop) {// length in bytes, defined
                 }
             }
             op = O_COMMA; goto push2; 
-        case '(': op = O_FUNC;
-            prec = priority(op);
+        case '(':
+            prec = priority(O_MEMBER);
             while (operp && prec <= priority(o_oper[operp-1])) {o_out[outp].val.type = T_OPER;o_out[outp].epoint=epoints[--operp];o_out[outp++].val.u.oper=o_oper[operp];}
             o_out[outp].val.type = T_OPER;
             o_out[outp].val.u.oper = O_PARENT;
             o_out[outp++].epoint=epoint;
             epoints[operp]=epoint;
-            o_oper[operp++] = op; lpoint++;
+            o_oper[operp++] = O_FUNC; lpoint++;
             continue;
-        case '[': op = O_INDEX;
-            prec = priority(op);
+        case '[':
+            prec = priority(O_MEMBER);
             while (operp && prec <= priority(o_oper[operp-1])) {o_out[outp].val.type = T_OPER;o_out[outp].epoint=epoints[--operp];o_out[outp++].val.u.oper=o_oper[operp];}
             o_out[outp].val.type = T_OPER;
             o_out[outp].val.u.oper = O_BRACKET;
             o_out[outp++].epoint=epoint;
             epoints[operp]=epoint;
-            o_oper[operp++] = op; lpoint++;
+            o_oper[operp++] = O_INDEX; lpoint++;
             continue;
         case '&': if (pline[lpoint+1] == '&') {lpoint++;op = O_LAND;} else op = O_AND; goto push2;
         case '|': if (pline[lpoint+1] == '|') {lpoint++;op = O_LOR;} else op = O_OR;goto push2;
