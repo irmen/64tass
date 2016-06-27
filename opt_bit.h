12204f010ad981fbbec5ec77d92b2fe15f5d1cb5
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2016-06-27 11:48:11 +0000

    Simplify. Or it depend ;)

---
+++ opt_bit.h
@@ -0,0 +1,50 @@
+/*
+    $Id$
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+#ifndef OPT_BIT_H
+#define OPT_BIT_H
+#include "inttypes.h"
+#include "stdbool.h"
+
+typedef struct Bit Bit;
+
+#define BU 0
+#define B0 1
+#define B1 2
+
+extern struct Bit *new_bit0(void);
+extern struct Bit *new_bit1(void);
+extern struct Bit *new_bitu(void);
+extern struct Bit *new_bit(uint8_t);
+extern void del_bit(struct Bit *);
+extern struct Bit *ref_bit(struct Bit *);
+extern struct Bit *inv_bit(Bit *);
+extern void mod_bit(struct Bit *, uint8_t);
+extern uint8_t get_bit(const struct Bit *);
+extern void reset_bit(struct Bit **v);
+extern bool eq_bit(const struct Bit *, const struct Bit *);
+extern bool neq_bit(const Bit *, const Bit *);
+extern struct Bit *add_bit(struct Bit *, struct Bit *, struct Bit *, struct Bit **);
+extern struct Bit *ld_bit(struct Bit *, struct Bit *);
+extern struct Bit *and_bit(struct Bit *, struct Bit *);
+extern struct Bit *or_bit(struct Bit *, struct Bit *);
+extern struct Bit *xor_bit(struct Bit *, struct Bit *);
+extern void init_opt_bit(void);
+extern void destroy_opt_bit(void);
+
+#endif
