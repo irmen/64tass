e08bb2278dc4a0fa97767bcc2779e942b1f290ec
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2016-11-26 11:12:26 +0000

    Duplicates append

--- variables.h
+++ variables.h
@@ -41,7 +41,7 @@ extern struct Label *find_label3(const str_t *, struct Namespace *, uint8_t);
 extern struct Label *find_anonlabel(int32_t);
 extern struct Label *find_anonlabel2(int32_t, struct Namespace *);
 extern struct Label *new_label(const str_t *, struct Namespace *, uint8_t, bool *);
-extern void labelprint(const struct symbol_output_s *);
+extern bool labelprint(const struct symbol_output_s *, bool);
 extern void shadow_check(struct Namespace *);
 extern void destroy_variables(void);
 extern void init_variables(void);
