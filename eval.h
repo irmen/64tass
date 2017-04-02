5f640bca816c191755e462e1308c8ec2d40bd863
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2017-04-02 08:54:27 +0000

    Simplify

--- eval.h
+++ eval.h
@@ -39,7 +39,6 @@ struct oper_s {
 typedef struct oper_s *oper_t;
 
 extern bool get_exp(int, struct file_s *, unsigned int, unsigned int, linepos_t);
-extern bool get_exp_var(struct file_s *, linepos_t);
 extern struct values_s *get_val(void);
 extern Obj *pull_val(struct linepos_s *);
 extern size_t get_val_remaining(void);
